1f7ae53ed993db20dcef67bd9fd826fefaff37c5
Felix.9127 <felix@gw2auth.com>
2021-09-26 05:12:59 +0200

    Update AbstractRestController to not expose details of unknown exceptions

--- src/main/java/com/gw2auth/oauth2/server/web/AbstractRestController.java
+++ src/main/java/com/gw2auth/oauth2/server/web/AbstractRestController.java
@@ -2,12 +2,16 @@ package com.gw2auth.oauth2.server.web;
 
 import com.gw2auth.oauth2.server.service.Gw2AuthServiceException;
 import com.gw2auth.oauth2.server.web.dto.ApiErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public abstract class AbstractRestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractRestController.class);
 
     @ExceptionHandler({Gw2AuthServiceException.class})
     public ResponseEntity<ApiErrorResponse> handleException(Gw2AuthServiceException exc) {
@@ -19,10 +23,11 @@ public abstract class AbstractRestController {
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<ApiErrorResponse> handleGenericException(Exception exc) {
+        LOG.error("Unhandled error while processing request", exc);
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(new ApiErrorResponse(exc.getClass().getSimpleName(), exc.getLocalizedMessage()));
+                .body(new ApiErrorResponse(exc.getClass().getSimpleName(), "An unknown error occured while processing your request"));
     }
-
 }
