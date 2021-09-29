b9ae3be138cf9d1bda894208076ef8a0f5f15d03
Felix.9127 <felix@gw2auth.com>
2021-09-29 04:09:20 +0200

    feature: Improve themed styling

--- src/main/angular/app/main/header/header.component.ts
+++ src/main/angular/app/main/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {AuthService} from "../../auth.service";
+import {DOCUMENT} from '@angular/common';
+
 
 @Component({
   selector: 'app-main-header',
@@ -7,13 +9,37 @@ import {AuthService} from "../../auth.service";
 })
 export class HeaderComponent implements OnInit {
 
-  isAuthenticated: boolean = false;
+  readonly SYSTEM = 'system';
+  readonly LIGHT = 'light';
+  readonly DARK = 'dark';
 
-  constructor(private authService: AuthService) {
+  isAuthenticated: boolean = false;
+  activeAppearance = this.SYSTEM;
+
+  constructor(@Inject(DOCUMENT) private readonly document: Document, private readonly authService: AuthService) {
   }
 
   ngOnInit(): void {
     this.authService.isAuthenticated().subscribe((isAuthenticated) => this.isAuthenticated = isAuthenticated);
+  }
+
+  onAppearanceClick(value: string): void {
+    if (value != this.activeAppearance) {
+      this.activeAppearance = value;
+
+      this.document.documentElement.classList.remove('theme-light', 'theme-dark');
+
+      switch (this.activeAppearance) {
+        case this.DARK: {
+          this.document.documentElement.classList.add('theme-dark');
+          break;
+        }
+        case this.LIGHT: {
+          this.document.documentElement.classList.add('theme-light');
+          break;
+        }
+      }
+    }
   }
 
   onLogoutClick(): void {
