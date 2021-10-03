f9760d54971820f58daa4c13fde336d74f958d35
Felix.9127 <felix@gw2auth.com>
2021-10-04 00:20:17 +0200

    ui: Changed alignment of text inside summary elements

--- src/main/angular/app/general/summary-element.component.ts
+++ src/main/angular/app/general/summary-element.component.ts
@@ -4,16 +4,18 @@ import {IconProp} from "@fortawesome/fontawesome-svg-core";
 @Component({
     selector: 'app-summary-element',
     template: `
-        <div class="card bg-accent">
-            <div class="card-header">
+        <div class="card bg-accent h-100">
+            <div class="card-header text-center">
                 <h3>
                     <fa-icon *ngIf="faIcon != null" [icon]="faIcon"></fa-icon>
                     <span *ngIf="faIcon != null" [textContent]="' '"></span>
                     {{title}}
                 </h3>
             </div>
-            <div class="card-body">
-                <ng-content></ng-content>
+            <div class="card-body d-flex align-items-center">
+                <div class="w-100 text-center">
+                    <ng-content></ng-content>
+                </div>
             </div>
         </div>
     `
