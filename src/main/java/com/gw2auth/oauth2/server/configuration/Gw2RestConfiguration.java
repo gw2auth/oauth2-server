554785341a06eff4d89c0b188fda2a72b6ebed8a
Felix.9127 <felix@gw2auth.com>
2021-10-04 03:51:30 +0200

    tech: Add metrics for gw2-rest-template

--- src/main/java/com/gw2auth/oauth2/server/configuration/Gw2RestConfiguration.java
+++ src/main/java/com/gw2auth/oauth2/server/configuration/Gw2RestConfiguration.java
@@ -1,5 +1,6 @@
 package com.gw2auth.oauth2.server.configuration;
 
+import org.springframework.boot.actuate.metrics.web.client.MetricsRestTemplateCustomizer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,12 @@ import java.time.Duration;
 public class Gw2RestConfiguration {
 
     @Bean("gw2-rest-template")
-    public RestTemplate gw2RestTemplate() {
+    public RestTemplate gw2RestTemplate(MetricsRestTemplateCustomizer metricsRestTemplateCustomizer) {
         return new RestTemplateBuilder()
                 .rootUri("https://api.guildwars2.com")
                 .setConnectTimeout(Duration.ofSeconds(3L))
                 .setReadTimeout(Duration.ofSeconds(5L))
+                .customizers(metricsRestTemplateCustomizer)
                 .build();
     }
 }
