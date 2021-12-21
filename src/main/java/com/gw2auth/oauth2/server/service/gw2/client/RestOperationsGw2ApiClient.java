292ef47400ba519bde64c0612542b0d97e02ac6f
Felix.9127 <felix@gw2auth.com>
2021-12-21 23:11:27 +0100

    fix: Use query placeholders instead for optimized monitoring

--- src/main/java/com/gw2auth/oauth2/server/service/gw2/client/RestOperationsGw2ApiClient.java
+++ src/main/java/com/gw2auth/oauth2/server/service/gw2/client/RestOperationsGw2ApiClient.java
@@ -10,6 +10,10 @@ import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RestOperationsGw2ApiClient implements Gw2ApiClient {
 
     private final RestOperations restOperations;
@@ -20,13 +24,28 @@ public class RestOperationsGw2ApiClient implements Gw2ApiClient {
 
     @Override
     public <T> ResponseEntity<T> get(String path, MultiValueMap<String, String> query, MultiValueMap<String, String> headers, TypeReference<T> typeReference) {
+        final UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(path);
+        final Map<String, String> params = new HashMap<>(query.size());
+        int i = 0;
+        String variableName;
+
+        for (Map.Entry<String, List<String>> entry : query.entrySet()) {
+            for (String value : entry.getValue()) {
+                variableName = "_" + (i++);
+
+                uriComponentsBuilder.queryParam(entry.getKey(), "{" + variableName + "}");
+                params.put(variableName, value);
+            }
+        }
+
         ResponseEntity<T> response;
         try {
             response = this.restOperations.exchange(
-                    UriComponentsBuilder.fromPath(path).queryParams(query).toUriString(),
+                    uriComponentsBuilder.build().toUriString(),
                     HttpMethod.GET,
                     new HttpEntity<>(headers),
-                    ParameterizedTypeReference.forType(typeReference.getType())
+                    ParameterizedTypeReference.forType(typeReference.getType()),
+                    params
             );
         } catch (RestClientResponseException e) {
             response = ResponseEntity.status(e.getRawStatusCode()).headers(e.getResponseHeaders()).build();
