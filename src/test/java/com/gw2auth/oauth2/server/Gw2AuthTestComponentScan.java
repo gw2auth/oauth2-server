72df7fac095acffe1507c69a6ea30666afb836e3
Felix.9127 <felix@gw2auth.com>
2022-01-08 21:21:16 +0100

    tech: Update hazelcast configuration

--- src/test/java/com/gw2auth/oauth2/server/Gw2AuthTestComponentScan.java
+++ src/test/java/com/gw2auth/oauth2/server/Gw2AuthTestComponentScan.java
@@ -1,7 +1,6 @@
 package com.gw2auth.oauth2.server;
 
 import com.gw2auth.oauth2.server.configuration.Gw2ApiClientConfiguration;
-import com.gw2auth.oauth2.server.configuration.HazelcastHttpSessionConfiguration;
 import com.gw2auth.oauth2.server.configuration.SelfProxyRestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -11,7 +10,7 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 @ComponentScan(basePackageClasses = Application.class, excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SelfProxyRestConfiguration.class, Gw2ApiClientConfiguration.class, HazelcastHttpSessionConfiguration.class})
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SelfProxyRestConfiguration.class, Gw2ApiClientConfiguration.class})
 })
 public @interface Gw2AuthTestComponentScan {
 
