f41d160372dbc8978395734d8f78f3e1c8c80dab
Felix <23635466+its-felix@users.noreply.github.com>
2023-10-12 14:09:22 +0800

    tech: use virtual thread executors for tomcat and spring

---
+++ src/main/java/com/gw2auth/oauth2/server/configuration/SpringThreadConfiguration.java
@@ -0,0 +1,23 @@
+package com.gw2auth.oauth2.server.configuration;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+public class SpringThreadConfiguration {
+
+    @Bean
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> tomcatProtocolHandlerCustomizer() {
+        return (protocolHandler) -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
+}
