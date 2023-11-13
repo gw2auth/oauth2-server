c4258dc28570e52ea79d406b1dcfabb10785d643
Felix <23635466+its-felix@users.noreply.github.com>
2023-11-13 23:08:47 +0100

    fix: add log to debug login on test instances

--- src/main/java/com/gw2auth/oauth2/server/configuration/OAuth2ClientConfiguration.java
+++ src/main/java/com/gw2auth/oauth2/server/configuration/OAuth2ClientConfiguration.java
@@ -1,7 +1,10 @@
 package com.gw2auth.oauth2.server.configuration;
 
 import com.gw2auth.oauth2.server.service.security.AuthenticationHelper;
+import com.gw2auth.oauth2.server.util.Pair;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.oauth2.client.ClientsConfiguredCondition;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesMapper;
@@ -16,9 +19,9 @@ import org.springframework.security.web.util.UrlUtils;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableConfigurationProperties(OAuth2ClientProperties.class)
@@ -32,14 +35,52 @@ public class OAuth2ClientConfiguration {
     }
 
     private record CustomClientRegistrationRepository(ClientRegistrationRepository base) implements ClientRegistrationRepository {
-        @Override
-            public ClientRegistration findByRegistrationId(String registrationId) {
-                final HttpServletRequest request = AuthenticationHelper.getCurrentRequest().orElseThrow();
-                final UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(UrlUtils.buildFullRequestUrl(request)).build();
 
-                return Optional.ofNullable(uriComponents.getHost())
-                        .map((host) -> this.base.findByRegistrationId(registrationId + "@" + host))
-                        .orElseGet(() -> this.base.findByRegistrationId(registrationId));
+        private static final Logger LOG = LoggerFactory.getLogger(CustomClientRegistrationRepository.class);
+
+        @Override
+        public ClientRegistration findByRegistrationId(String registrationId) {
+            final HttpServletRequest request = AuthenticationHelper.getCurrentRequest().orElseThrow();
+            final UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(UrlUtils.buildFullRequestUrl(request)).build();
+
+            LOG.info("resolved UriComponents: {} (host={})", uriComponents, uriComponents.getHost());
+            try {
+                logRequest(request);
+            } catch (Exception e) {
+                LOG.warn("failed to log request", e);
             }
+
+            return Optional.ofNullable(uriComponents.getHost())
+                    .flatMap((host) -> findBase(registrationId + "@" + host))
+                    .or(() -> findBase(registrationId))
+                    .orElse(null);
         }
+
+        private Optional<ClientRegistration> findBase(String registrationId) {
+            return Optional.ofNullable(this.base.findByRegistrationId(registrationId));
+        }
+
+        private static void logRequest(HttpServletRequest request) {
+            final Map<String, List<String>> headers = Stream.of("X-Forwarded-For", "X-Forwarded-Host", "Forwarded", "Host")
+                    .flatMap((v) -> {
+                        final Enumeration<String> enumeration = request.getHeaders(v);
+                        if (enumeration == null) {
+                            return Stream.empty();
+                        }
+
+                        final List<String> values = new ArrayList<>();
+                        final Iterator<String> it = enumeration.asIterator();
+                        while (it.hasNext()) {
+                            values.add(it.next());
+                        }
+
+                        return Stream.of(new Pair<>(v, values));
+                    })
+                    .collect(Collectors.toMap(Pair::v1, Pair::v2));
+
+            final String serverName = request.getServerName();
+
+            LOG.info("ClientRegistrationRepository; request with serverName={} and headers={}", serverName, headers);
+        }
+    }
 }
