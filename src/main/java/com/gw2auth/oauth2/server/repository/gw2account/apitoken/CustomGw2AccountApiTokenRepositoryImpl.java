a8eb617308c79e20599513b8661256bd524e5d75
Felix <23635466+its-felix@users.noreply.github.com>
2024-08-07 23:53:24 +0200

    Revert "feat: update gw2_api_permissions_bit_set in scheduled check"
    This reverts commit da21469cf0c768da3e2abd040e82458c1d2ef3f6.

--- src/main/java/com/gw2auth/oauth2/server/repository/gw2account/apitoken/CustomGw2AccountApiTokenRepositoryImpl.java
+++ src/main/java/com/gw2auth/oauth2/server/repository/gw2account/apitoken/CustomGw2AccountApiTokenRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.gw2auth.oauth2.server.repository.gw2account.apitoken;
 
-import com.gw2auth.oauth2.server.service.Gw2ApiPermission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
@@ -17,17 +16,14 @@ public class CustomGw2AccountApiTokenRepositoryImpl implements CustomGw2AccountA
 
     private static final String QUERY_VALID = """
     UPDATE gw2_account_api_tokens
-    SET last_valid_time = :last_valid_check_time,
-    last_valid_check_time = :last_valid_check_time,
-    gw2_api_permissions_bit_set = IF(COALESCE( :gw2_api_permissions_bit_set , 0) = 0, gw2_api_permissions_bit_set, :gw2_api_permissions_bit_set )
+    SET last_valid_time = :last_valid_check_time, last_valid_check_time = :last_valid_check_time
     WHERE account_id = :account_id
     AND gw2_account_id = :gw2_account_id
     """;
 
     private static final String QUERY_INVALID = """
     UPDATE gw2_account_api_tokens
-    SET last_valid_check_time = :last_valid_check_time,
-    gw2_api_permissions_bit_set = IF(COALESCE( :gw2_api_permissions_bit_set , 0) = 0, gw2_api_permissions_bit_set, :gw2_api_permissions_bit_set )
+    SET last_valid_check_time = :last_valid_check_time
     WHERE account_id = :account_id
     AND gw2_account_id = :gw2_account_id
     """;
@@ -49,7 +45,6 @@ public class CustomGw2AccountApiTokenRepositoryImpl implements CustomGw2AccountA
             final SqlParameterSource sqlParameterSource = new MapSqlParameterSource(Map.of(
                     "account_id", entity.accountId(),
                     "gw2_account_id", entity.gw2AccountId(),
-                    "gw2_api_permissions_bit_set", Optional.ofNullable(entity.gw2ApiPermissions()).map(Gw2ApiPermission::toBitSet).orElse(0),
                     "last_valid_check_time", Timestamp.from(lastValidCheckTime)
             ));
 
--- src/main/java/com/gw2auth/oauth2/server/repository/gw2account/apitoken/Gw2AccountApiTokenValidUpdateEntity.java
+++ src/main/java/com/gw2auth/oauth2/server/repository/gw2account/apitoken/Gw2AccountApiTokenValidUpdateEntity.java
@@ -1,9 +1,6 @@
 package com.gw2auth.oauth2.server.repository.gw2account.apitoken;
 
-import com.gw2auth.oauth2.server.service.Gw2ApiPermission;
-
-import java.util.Set;
 import java.util.UUID;
 
-public record Gw2AccountApiTokenValidUpdateEntity(UUID accountId, UUID gw2AccountId, Set<Gw2ApiPermission> gw2ApiPermissions, boolean isValid) {
+public record Gw2AccountApiTokenValidUpdateEntity(UUID accountId, UUID gw2AccountId, boolean isValid) {
 }
