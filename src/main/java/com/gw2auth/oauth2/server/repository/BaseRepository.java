4ef857f14a88f1cd01b368993841173f0f1f1f89
Felix <23635466+its-felix@users.noreply.github.com>
2024-09-05 03:16:42 +0200

    feat: enable transaction retry for CockroachDB 410001 state

--- src/main/java/com/gw2auth/oauth2/server/repository/BaseRepository.java
+++ src/main/java/com/gw2auth/oauth2/server/repository/BaseRepository.java
@@ -2,8 +2,15 @@ package com.gw2auth.oauth2.server.repository;
 
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 
 @NoRepositoryBean
+@Retryable(
+        exceptionExpression = "@cockroachExceptionClassifier.shouldRetry(#root)",
+        maxAttempts = 5,
+        backoff = @Backoff(delay = 500, multiplier = 1.5, maxDelay = 5000)
+)
 public interface BaseRepository<T> extends Repository<T, Void> {
 
     T save(T entity);
