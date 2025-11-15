034c6221afb1ceba29527bdee03cc77c216ed27c
Felix <23635466+its-felix@users.noreply.github.com>
2025-11-15 18:28:14 +0100

    fix: shell syntax for entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,14 @@
 
 java_args=""
 
-if [[ -n $JAVA_OPTS ]]; then
+if [ -n "$JAVA_OPTS" ]; then
   java_args="${JAVA_OPTS} "
 fi
 
 java_args="${java_args}-jar /opt/app/application.jar"
 
-if [[ -n $APPLICATION_ARGS ]]; then
+if [ -n "$APPLICATION_ARGS" ]; then
   java_args="${java_args} ${APPLICATION_ARGS}"
 fi
 
-java $java_args
+exec java $java_args
